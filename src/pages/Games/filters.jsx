5c0e18162b875750defb8509be86587741207a08
Глеб Бодруг <110567737+Gudleif90@users.noreply.github.com>
2024-05-16 20:28:39 +0300

    Add files via upload

---
+++ src/pages/Games/filters.jsx
@@ -0,0 +1,10 @@
+import { FaInbox, FaStar, FaCalendarAlt } from "react-icons/fa";
+
+import React from "react";
+
+const filters = [
+  { name: "wishlist", icon: <FaInbox /> },
+  { name: "release date", icon: <FaCalendarAlt /> },
+];
+
+export default filters;
---
+++ src/pages/Games/genres.jsx
@@ -0,0 +1,22 @@
+import {
+  GiBoxingGlove,
+  GiChessKnight,
+  GiPistolGun,
+  GiCapeArmor,
+  GiCheckeredFlag,
+  GiSoccerBall,
+} from "react-icons/gi";
+import { FaPuzzlePiece, FaMountain } from "react-icons/fa";
+import React from "react";
+
+const genres = [
+  { name: "action", icon: <GiBoxingGlove /> },
+  { name: "rpg", icon: <GiCapeArmor /> },
+  { name: "shooter", icon: <GiPistolGun /> },
+  { name: "adventure", icon: <FaMountain /> },
+  { name: "puzzle", icon: <FaPuzzlePiece /> },
+  { name: "racing", icon: <GiCheckeredFlag /> },
+  { name: "sports", icon: <GiSoccerBall /> },
+];
+
+export default genres;
